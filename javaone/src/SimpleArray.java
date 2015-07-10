164be83045540de68a90e5931d5d912f7e14a9eb
prem <prsagar2003@yahoo.com>
2015-07-10 16:21:01 -0400

    COmmit 4

--- javaone/src/SimpleArray.java
+++ javaone/src/SimpleArray.java
@@ -16,6 +16,7 @@ public class SimpleArray {
 	    emps.add(new Employee("Ponnuswami", "Ram"));
 	    emps.add(new Employee("Chava", "Manoj"));
 	    emps.add(new Employee("Sterling", "Debbie"));
+	    emps.add(new Employee("Lisette","Lisett"));
 	    System.out.println("Team : \n");
 	    for(int i=0;i<emps.size();i++)
 	    {
